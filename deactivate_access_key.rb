b532b65709e34cce0ba6813c043c4f89cce48d12
Matei Adrian <adrian.m.matei@gmail.com>
2019-01-21 15:20:08 +0200

    Add deactivate access key lambda

---
+++ deactivate_access_key.rb
@@ -0,0 +1,21 @@
+require 'aws-sdk-iam'
+require 'json'
+
+def lambda_handler(event:, context:)
+    detail = event['detail']
+    iam = Aws::IAM::Client.new(region: 'us-east-2')
+
+    if detail['service']['serviceName'] == 'guardduty' && detail['service']['resourceRole'] == "TARGET" && detail['resource']['resourceType'] == "AccessKey" && detail['severity'] >= 5
+        access_details = detail['resource']['accessKeyDetails']
+
+        begin
+            iam.update_access_key({
+                access_key_id: access_details['accessKeyId'], 
+                status: "Inactive",
+                user_name: access_details['userName'], 
+            })
+        rescue Aws::IAM::Errors::ServiceError => e
+          raise "Deactivate IAM user access key error: #{e}"
+        end
+    end
+end

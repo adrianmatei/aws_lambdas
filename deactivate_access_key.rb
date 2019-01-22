50cb41031a3298d3c981d2af032aef89b97e2511
Matei Adrian <adrian.m.matei@gmail.com>
2019-01-22 15:16:42 +0200

    Reformat and remove region

--- deactivate_access_key.rb
+++ deactivate_access_key.rb
@@ -2,20 +2,20 @@ require 'aws-sdk-iam'
 require 'json'
 
 def lambda_handler(event:, context:)
-    detail = event['detail']
-    iam = Aws::IAM::Client.new(region: 'us-east-2')
+  detail = event['detail']
+  iam = Aws::IAM::Client.new
 
-    if detail['service']['serviceName'] == 'guardduty' && detail['service']['resourceRole'] == "TARGET" && detail['resource']['resourceType'] == "AccessKey" && detail['severity'] >= 5
-        access_details = detail['resource']['accessKeyDetails']
+  if detail['service']['serviceName'] == 'guardduty' && detail['service']['resourceRole'] == "TARGET" && detail['resource']['resourceType'] == "AccessKey" && detail['severity'] >= 5
+    access_details = detail['resource']['accessKeyDetails']
 
-        begin
-            iam.update_access_key({
-                access_key_id: access_details['accessKeyId'], 
-                status: "Inactive",
-                user_name: access_details['userName'], 
-            })
-        rescue Aws::IAM::Errors::ServiceError => e
-          raise "Deactivate IAM user access key error: #{e}"
-        end
+    begin
+      iam.update_access_key({
+        access_key_id: access_details['accessKeyId'],
+        status: "Inactive",
+        user_name: access_details['userName'],
+      })
+    rescue Aws::IAM::Errors::ServiceError => e
+      raise "Deactivate IAM user access key error: #{e}"
     end
+  end
 end

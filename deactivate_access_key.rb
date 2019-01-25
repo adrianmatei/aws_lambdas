f98c406c2a0e426596dbfa964098eb1a85fe51be
adrianmatei <adrian.m.matei@gmail.com>
2019-01-25 12:20:14 +0200

    Add SNS notifications to lambdas

--- deactivate_access_key.rb
+++ deactivate_access_key.rb
@@ -1,21 +1,45 @@
-require 'aws-sdk-iam'
-require 'json'
+# set lambda timeout to 15 seconds
+require 'aws-sdk'
 
 def lambda_handler(event:, context:)
+  region = "" # required
   detail = event['detail']
-  iam = Aws::IAM::Client.new(region: '')
 
   if detail['service']['serviceName'] == 'guardduty' && detail['service']['resourceRole'] == "TARGET" && detail['resource']['resourceType'] == "AccessKey" && detail['severity'] >= 5
     access_details = detail['resource']['accessKeyDetails']
 
     begin
+      iam = Aws::IAM::Client.new(region: region)
       iam.update_access_key({
         access_key_id: access_details['accessKeyId'],
         status: "Inactive",
         user_name: access_details['userName'],
       })
+
+      send_notification(detail, region)
     rescue Aws::IAM::Errors::ServiceError => e
       raise "Deactivate IAM user access key error: #{e}"
     end
+  end
+end
+
+private
+
+def send_notification(detail, region)
+  begin
+    sns = Aws::SNS::Client.new(region: region)
+    sns_topic_arn = "" # required
+    subject = "GuardDuty: IAM user access key was disabled"
+
+    message = "The access key was deactivated for user #{detail['resource']['accessKeyDetails']['userName']}. \n\n"
+    message += "Reason:\n#{detail['title']}\n#{detail['description']}"
+
+    sns.publish({
+      topic_arn: sns_topic_arn,
+      message: message,
+      subject: subject
+    })
+  rescue Aws::SNS::Errors::ServiceError => e
+    raise "GuardDuty to SNS send e-mail error: #{e}"
   end
 end
--- guard_duty_stop_instance.rb
+++ guard_duty_stop_instance.rb
@@ -1,25 +1,55 @@
-require 'aws-sdk-ec2'
-require 'json'
+# set lambda timeout to 15 seconds
+require 'aws-sdk'
 
 def lambda_handler(event:, context:)
-  ec2 = Aws::EC2::Client.new(region: '')
-  instance_id = event['detail']['resource']['instanceDetails']['instanceId']
+  region = "" # required
+  detail = event['detail']
 
+  if detail['service']['serviceName'] == 'guardduty' && detail['service']['resourceRole'] == "TARGET" && detail['resource']['resourceType'] == "Instance"
+    instance_id = detail['resource']['instanceDetails']['instanceId']
+
+    begin
+      ec2 = Aws::EC2::Client.new(region: 'us-east-2')
+
+      # rename the compromised instance
+      ec2.create_tags({
+        resources: [instance_id],
+        tags: [
+          {
+            key: "Name",
+            value: "DANGER - DO NOT RESTART - COMPROMISED",
+          }
+        ]
+      })
+
+      # stop the compromised instance
+      ec2.stop_instances({ instance_ids: [instance_id] })
+
+      # send sns e-mail
+      send_notification(detail, region)
+    rescue Aws::EC2::Errors::ServiceError => e
+      raise "Stop EC2 Instance error: #{e}"
+    end
+  end
+end
+
+private
+
+def send_notification(detail, region)
   begin
-    # rename the compromised instance
-    ec2.create_tags({
-      resources: [instance_id],
-      tags: [
-        {
-          key: "Name",
-          value: "DANGER - DO NOT RESTART - COMPROMISED",
-        }
-      ]
-    })
+    sns = Aws::SNS::Client.new(region: region)
+    sns_topic_arn = "" # required
+    subject = "GuardDuty: Compromised EC2 instance was stopped"
 
-    # stop the compromised instance
-    ec2.stop_instances({ instance_ids: [instance_id] })
-  rescue Aws::EC2::Errors::ServiceError => e
-    raise "Stop EC2 Instance error: #{e}"
+    message = "The instance #{detail['resource']['instanceDetails']['instanceId']} was stopped and tagged as compromised.\n"
+    message += "Reason:\n\n#{detail['title']}\n#{detail['description']}"
+
+    sns.publish({
+      topic_arn: sns_topic_arn,
+      message: message,
+      subject: subject
+    })
+  rescue Aws::SNS::Errors::ServiceError => e
+    raise "GuardDuty to SNS send e-mail error: #{e}"
   end
 end
